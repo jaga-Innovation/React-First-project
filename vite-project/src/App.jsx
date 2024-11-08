0e50c9bfefd97f0b42fe88508dda07276a59a3ec
Jagadeshwaran <148187521+jaga-Innovation@users.noreply.github.com>
2024-11-08 22:37:29 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="javascript:void(0)" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
