71360784e9f1f8e34099039b60b72d3b967d5463
Jagadeshwaran <148187521+jaga-Innovation@users.noreply.github.com>
2024-11-08 22:31:02 +0530

    Update User.jsx

--- vite-project/src/User.jsx
+++ vite-project/src/User.jsx
@@ -2,11 +2,12 @@
 // /* eslint-disable react/prop-types */
 // import React from 'react'
 
-const User = ({ name, age }) => {
+const User = ({ name, age,  }) => {
   return (
     <div>
       <h1>name = {name}</h1>
       <h2>Age= {age}</h2>
+      <h2>Mobile number = {}</h2>
     </div>
   );
 };
